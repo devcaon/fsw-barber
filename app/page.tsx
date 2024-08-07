0d07c662326134e60f39aed183b39d9ae22f9c0e
Claudio Oliveira <dev.caon@gmail.com>
2024-08-07 17:20:35 -0300

    feat: added initial search component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,30 @@
+import { SearchIcon } from "lucide-react";
 import Header from "./_components/Header";
+import { Button } from "./_components/ui/button";
+import { Input } from "./_components/ui/input";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <Header />
+    <div className="">
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Cláudio</h2>
+        <p>Segunda-feira, 05 de Agosto</p>
+
+        {/* search */}
+        <div className="flex items-center gap-2 mt-6">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        {/* banner */}
+        <div className="relative w-full h-[160px] mt-6">
+          <Image src='/banner_01.png' alt="Agende nos melhores com FSW Barber" className="object-cover  rounded-lg" fill />
+        </div>
+      </div>
+    </div>
   );
 }
